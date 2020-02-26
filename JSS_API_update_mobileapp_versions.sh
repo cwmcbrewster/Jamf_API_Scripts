3fa32a83b5c70aced083824254cc18a825232a6d
cwmcbrewster <43007934+cwmcbrewster@users.noreply.github.com>
2020-02-26 10:40:57 -0800

    Update JSS_API_update_mobileapp_versions.sh

--- JSS_API_update_mobileapp_versions.sh
+++ JSS_API_update_mobileapp_versions.sh
@@ -3,7 +3,7 @@
 #loosly based on work by https://github.com/bumbletech
 
 ####Change these variables for your enviornment####
-JSSurl="https://yourjss:8443"
+JSSurl='https://yourjss:8443'
 apiUser='user'
 apiPass='pass'
 
@@ -13,18 +13,18 @@ apiPass='pass'
 
 #check to see if variables have been changed
 if [ "$JSSurl" == "https://yourjss:8443" ] || [ "$apiUser" == "user" ] || [ "$apiPass" == "pass" ]; then
-	echo "One of the variables needed for your JSS and accounts is set to the default. Open this script in a text editor and check the variables for JSSurl, apiReadOnlyUser & apiReadOnlyPass and try again."
-	echo "Exiting..."
-	exit 1
+  echo "One of the variables needed for your JSS and accounts is set to the default. Open this script in a text editor and check the variables for JSSurl, apiReadOnlyUser & apiReadOnlyPass and try again."
+  echo "Exiting..."
+  exit 1
 fi
 
 #check for jq
 if [ ! -f /usr/local/bin/jq ]
 then
-	echo "jq (https://stedolan.github.io/jq/) could not be found. jq is needed to parse iTunes API results. Please install jq and try again."
-	echo "If you use Homebrew, run 'brew install jq'."
-	echo "Exiting..."
-	exit 1
+  echo "jq (https://stedolan.github.io/jq/) could not be found. jq is needed to parse iTunes API results. Please install jq and try again."
+  echo "If you use Homebrew, run 'brew install jq'."
+  echo "Exiting..."
+  exit 1
 fi
 
 #set the path for the JSS to check
@@ -39,39 +39,39 @@ function check_for_updates () {
 
 while read xml_string; do
 
-	#set the app info from the JSS
-	id=$(echo "$xml_string" | awk -F'<id>|</id>' '{print $2}')
-	app_bundle_id=$(echo "$xml_string" | awk -F'<bundle_id>|</bundle_id>' '{print $2}')
-	jamf_version=$(echo "$xml_string" | awk -F'<version>|</version>' '{print $2}')
-	itunes_lastknown_url_raw=$(curl -s -H "Accept: text/xml" -u ${apiUser}:"${apiPass}" ${JSSapiPath}/id/$id/subset/General | xpath '//mobile_device_application/general/itunes_store_url' 2>&1 | awk -F'<itunes_store_url>|</itunes_store_url>' '{print $2}' | grep .)
-	#XML can't deal with "&". replace the escape text.
-	itunes_lastknown_url="${itunes_lastknown_url_raw/&amp;/&}"
-	#itunesAdamId=$(echo $itunes_lastknown_url | sed -e 's/.*\/id\(.*\)?.*/\1/')
-	itunesAdamId=$(echo ${itunes_lastknown_url} | grep -o -E 'id\d{9,}' | awk -F'id' '{print $2}')
+  #set the app info from the JSS
+  id=$(echo "$xml_string" | awk -F'<id>|</id>' '{print $2}')
+  app_bundle_id=$(echo "$xml_string" | awk -F'<bundle_id>|</bundle_id>' '{print $2}')
+  jamf_version=$(echo "$xml_string" | awk -F'<version>|</version>' '{print $2}')
+  itunes_lastknown_url_raw=$(curl -s -H "Accept: text/xml" -u ${apiUser}:"${apiPass}" ${JSSapiPath}/id/$id/subset/General | xpath '//mobile_device_application/general/itunes_store_url' 2>&1 | awk -F'<itunes_store_url>|</itunes_store_url>' '{print $2}' | grep .)
+  #XML can't deal with "&". replace the escape text.
+  itunes_lastknown_url="${itunes_lastknown_url_raw/&amp;/&}"
+  #itunesAdamId=$(echo $itunes_lastknown_url | sed -e 's/.*\/id\(.*\)?.*/\1/')
+  itunesAdamId=$(echo ${itunes_lastknown_url} | grep -o -E 'id\d{9,}' | awk -F'id' '{print $2}')
 
-	#define itunes api lookup path with bundleID from JSS
-	itunes_api_url="https://uclient-api.itunes.apple.com/WebObjects/MZStorePlatform.woa/wa/lookup?version=2&id=${itunesAdamId}&p=mdm-lockup&caller=MDM&platform=itunes&cc=us&l=en"
+  #define itunes api lookup path with bundleID from JSS
+  itunes_api_url="https://uclient-api.itunes.apple.com/WebObjects/MZStorePlatform.woa/wa/lookup?version=2&id=${itunesAdamId}&p=mdm-lockup&caller=MDM&platform=itunes&cc=us&l=en"
 
-	#json results from itunes lookup
-	itunes_data=$(curl -s -H "Accept: application/JSON" -X GET "${itunes_api_url}")
-	bundleId=$(/usr/local/bin/jq -r ".results.\"${itunesAdamId}\".bundleId" <<< "${itunes_data}")
-	appleVersion=$(/usr/local/bin/jq -r ".results.\"${itunesAdamId}\".offers[].version.display" <<< "${itunes_data}" 2>/dev/null)
+  #json results from itunes lookup
+  itunes_data=$(curl -s -H "Accept: application/JSON" -X GET "${itunes_api_url}")
+  bundleId=$(/usr/local/bin/jq -r ".results.\"${itunesAdamId}\".bundleId" <<< "${itunes_data}")
+  appleVersion=$(/usr/local/bin/jq -r ".results.\"${itunesAdamId}\".offers[].version.display" <<< "${itunes_data}" 2>/dev/null)
 
   #check if app's bundleID matches what's on the JSS. If it's blank, there's no record on the iTunes store.
   if [[ -z $id ]]; then
     #do nothing. This is just a blank line in the data parsed from the jss
-  	:
+    :
   elif [[ ${app_bundle_id} != ${bundleId} ]]; then
     echo "Apple bundle ID:${app_bundle_id} | Jamf App ID:${id} | status:NO LONGER AVAILABLE"
   elif [[ ${jamf_version} != ${appleVersion} ]]; then
     echo "Apple bundle ID:${app_bundle_id} | Jamf App ID:${id} | status:VERSION MISMATCH"
-		read -p "Press Y to update ${app_bundle_id} from version ${jamf_version} to version ${appleVersion} [Y]: " continue </dev/tty
+    read -p "Press Y to update ${app_bundle_id} from version ${jamf_version} to version ${appleVersion} [Y]: " continue </dev/tty
     continue=${continue:-Y}
-		if [[ "${continue}" =~ [Y|y] ]]; then
+    if [[ "${continue}" =~ [Y|y] ]]; then
       versionData="<mobile_device_application><general><version>${appleVersion}</version></general></mobile_device_application>"
-		  curl -s -H "Content-Type: text/xml" -u ${apiUser}:${apiPass} "${JSSapiPath}/id/${id}/subset/General" -d "${versionData}" -X PUT
+      curl -s -H "Content-Type: text/xml" -u ${apiUser}:${apiPass} "${JSSapiPath}/id/${id}/subset/General" -d "${versionData}" -X PUT
       echo ""
-		fi
+    fi
   elif [[ -n ${id} ]]; then
     echo "Apple bundle ID:${app_bundle_id} | Jamf App ID:${id} | status:CURRENT"
   fi
